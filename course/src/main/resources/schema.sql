c9a4294f38deb3a7a35811665ad5085a66fb5ff3
Mohammed Samir <mohammed.samir@ukg.com>
2024-08-21 10:56:39 +0530

    schema changes

--- course/src/main/resources/schema.sql
+++ course/src/main/resources/schema.sql
@@ -1,8 +1,15 @@
-CREATE TABLE IF NOT EXISTS `course` (
+CREATE TABLE IF NOT EXISTS `Course` (
     `course_id` INT AUTO_INCREMENT PRIMARY KEY,
     `title` VARCHAR(30) NOT NULL,
     `duration` VARCHAR(30) NOT NULL,
     `uploaded_by` INT NOT NULL,
     `category` VARCHAR(10) NOT NULL,
     `is_approved` BIT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS `joinerXcourse` (
+    `joiner_id` INT,
+    `course_id` INT,
+    `is_completed` BIT NOT NULL,
+    PRIMARY KEY (`joiner_id`, `course_id`)
 );
--- query/src/main/resources/schema.sql
+++ query/src/main/resources/schema.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS `query` (
     `query_id` INT AUTO_INCREMENT PRIMARY KEY,
-    `description` VARCHAR(30) NOT NULL,
-    `response` VARCHAR(30) NOT NULL,
     `joiner_id` INT NOT NULL,
-    `mentor_id` INT NOT NULL
+    `mentor_id` INT NOT NULL,
+    `description` VARCHAR(30) NOT NULL,
+    `is_resolved` BIT NOT NULL,
+    `response` VARCHAR(30) NOT NULL
 );
